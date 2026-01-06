5b4dcbaf6981add156966869d3db26edaabca6c1
Sushant Jogdand <sushantjogdand6820@gmail.com>
2026-01-06 15:18:21 +0530

    Add auth routes and login/register controllers

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,14 @@
 import User from "../models/userModel.js";
 
+import bcrypt from "bcryptjs"
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
+
+dotenv.config();
+
+const jwt_secret = process.env.JWT_SECRET;
+
+
 
 export const registerController = async(req, res)=>{
     try {
@@ -15,7 +24,9 @@ export const registerController = async(req, res)=>{
             return res.status(409).json({success: false, message : "Email already registered please login"})
         }
 
-        const user = new User({userName, email, password, address, phone});
+        const hashedPassword =await bcrypt.hash(password,10)
+
+        const user = new User({userName, email, password : hashedPassword, address, phone});
 
         await user.save();
 
@@ -29,5 +40,52 @@ export const registerController = async(req, res)=>{
         console.log("Login error : ", error);
 
         return res.status(500).json({success: false, message: "error in register API"})
+    }
+}
+
+export const loginController = async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message : "please provide email or password"
+            })
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({
+                success : false,
+                message : "user not found"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            return res.status(500).json({
+                success : false,
+                message : "invalid credentials"
+            })
+        }
+
+        const token = jwt.sign({id: user._id}, jwt_secret)
+
+        user.password = undefined;
+        return res.status(200).json({
+            success: true,
+            message : "Login successfull",
+            token,
+            user
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message : "error in login api"
+        })
     }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerController } from "../controllers/authControllers.js";
+import { loginController, registerController } from "../controllers/authControllers.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", registerController)
+////REGISTER
+authRouter.post("/register", registerController);
+
+////LOGIN
+authRouter.post("/login", loginController);
+
 
 
 export default authRouter;
